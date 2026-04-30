42f245323b7897c81daa5a900dae2b3803b86268
Bob Shell <bob@example.com>
2026-04-30 09:50:25 -0400

    increment args

--- net/adoptopenjdk/bumblebench/compare/CompareUnsignedInt.java
+++ net/adoptopenjdk/bumblebench/compare/CompareUnsignedInt.java
@@ -5,15 +5,19 @@ import net.adoptopenjdk.bumblebench.core.MicroBench;
 public final class CompareUnsignedInt extends MicroBench {
     private static final int A_DEFAULT = -1;
     private static final int B_DEFAULT = 1;
-    private static final int A_VALUE = option("aValue", A_DEFAULT);
-    private static final int B_VALUE = option("bValue", B_DEFAULT);
+    private static final int A_INC = -5;
+    private static final int B_INC = 15;
+    private static volatile int a = option("a", A_DEFAULT);
+    private static volatile int b = option("b", B_DEFAULT);
 
     private static volatile int value = 0;
 
     @Override
     protected long doBatch(long numIterations) throws InterruptedException {
         for (long i = 0; i < numIterations; i++) {
-            value += Integer.compareUnsigned(A_VALUE, B_VALUE);
+            value += Integer.compareUnsigned(a, b);
+            a += A_INC;
+            b += B_INC;
         }
         return numIterations;
     } 
