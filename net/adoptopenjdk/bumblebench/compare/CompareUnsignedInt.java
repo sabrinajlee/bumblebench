ab36e8b1426e14f994247c5de95fdaf622aafea8
Bob Shell <bob@example.com>
2026-05-11 16:21:25 -0400

    exception on values outside range

--- net/adoptopenjdk/bumblebench/compare/CompareUnsignedInt.java
+++ net/adoptopenjdk/bumblebench/compare/CompareUnsignedInt.java
@@ -11,13 +11,22 @@ public final class CompareUnsignedInt extends MicroBench {
     private static volatile int b = option("b", B_DEFAULT);
 
     private static volatile int value = 0;
+    private static volatile int res = 0;
 
     @Override
     protected long doBatch(long numIterations) throws InterruptedException {
         for (long i = 0; i < numIterations; i++) {
-            value += Integer.compareUnsigned(a, b);
-            a += A_INC;
-            b += B_INC;
+            try {
+                res = Integer.compareUnsigned(a, b);
+                if (res < -1 || res > 1){
+                    throw new Exception("Returned value is not in range [-1,1]");
+                }
+                value += res;
+                a += A_INC;
+                b += B_INC;
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return numIterations;
     } 
