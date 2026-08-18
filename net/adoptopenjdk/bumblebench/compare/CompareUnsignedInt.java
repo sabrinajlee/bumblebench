e9de52e6b3d72342dbef49f770c45526bc7e37aa
Sabrina Lee <137124468+sabrinajlee@users.noreply.github.com>
2026-08-18 13:51:55 -0400

    jar for & 0x0F

--- net/adoptopenjdk/bumblebench/compare/CompareUnsignedInt.java
+++ net/adoptopenjdk/bumblebench/compare/CompareUnsignedInt.java
@@ -4,7 +4,7 @@ import net.adoptopenjdk.bumblebench.core.MicroBench;
 
 public final class CompareUnsignedInt extends MicroBench {
     // 16 values evenly spaced across the full unsigned int range.
-    // Power-of-2 length so (i + k) % 15 is a single AND, no division.
+    // Power-of-2 length so (i + k) & 0x0F is a single AND, no division.
     private static final int[] VALUES = {
         0x00000000, 0x11111111, 0x22222222, 0x33333333,
         0x44444444, 0x55555555, 0x66666666, 0x77777777,
@@ -23,56 +23,55 @@ public final class CompareUnsignedInt extends MicroBench {
 
         for (int i = 0; i < numIterations; i++) {
             int temp = 0;
-            temp += Integer.compareUnsigned(local_a, vals[ i        % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  1)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  2)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  3)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  4)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  5)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  6)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  7)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  8)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  9)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 10)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 11)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 12)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 13)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 14)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 15)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[ i        % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  1)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  2)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  3)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  4)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  5)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  6)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  7)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  8)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  9)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 10)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 11)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 12)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 13)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 14)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 15)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[ i        % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  1)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  2)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  3)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  4)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  5)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  6)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  7)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  8)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  9)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 10)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 11)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 12)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 13)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 14)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 15)  % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[ i        % 15]);
-            temp += Integer.compareUnsigned(local_a, vals[(i +  1)  % 15]);
+            temp += Integer.compareUnsigned(local_a, vals[ i        & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  1)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  2)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  3)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  4)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  5)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  6)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  7)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  8)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  9)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 10)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 11)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 12)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 13)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 14)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 15)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[ i        & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  1)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  2)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  3)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  4)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  5)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  6)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  7)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  8)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  9)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 10)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 11)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 12)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 13)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 14)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 15)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[ i        & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  1)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  2)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  3)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  4)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  5)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  6)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  7)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  8)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i +  9)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 10)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 11)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 12)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 13)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 14)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[(i + 15)  & 0x0F]);
+            temp += Integer.compareUnsigned(local_a, vals[ i        & 0x0F]);
             local_val += temp;
             local_a += 0x9e3779b9;
         }
