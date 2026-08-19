81ca9c33de6e8552b516fa9d76e302c91f0d39f5
Sabrina Lee <137124468+sabrinajlee@users.noreply.github.com>
2026-08-19 13:04:34 -0400

    10 and jar

--- net/adoptopenjdk/bumblebench/compare/CompareUnsignedInt.java
+++ net/adoptopenjdk/bumblebench/compare/CompareUnsignedInt.java
@@ -33,8 +33,6 @@ public final class CompareUnsignedInt extends MicroBench {
             temp += Integer.compareUnsigned(local_a, vals[(i +  7)  & 0x0F]);
             temp += Integer.compareUnsigned(local_a, vals[(i +  8)  & 0x0F]);
             temp += Integer.compareUnsigned(local_a, vals[(i +  9)  & 0x0F]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 10)  & 0x0F]);
-            temp += Integer.compareUnsigned(local_a, vals[(i + 11)  & 0x0F]);
             local_val += temp;
             local_a += 0x9e3779b9;
         }
